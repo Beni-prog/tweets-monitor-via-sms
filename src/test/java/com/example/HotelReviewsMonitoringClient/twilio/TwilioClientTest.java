1811947b570df429c8c5a39d3f55463bb72dd4a3
Acalinei Beniamin <60926121+Beni-prog@users.noreply.github.com>
2022-10-11 16:14:58 +0300

    Update TwilioClientTest.java

--- src/test/java/com/example/HotelReviewsMonitoringClient/twilio/TwilioClientTest.java
+++ src/test/java/com/example/HotelReviewsMonitoringClient/twilio/TwilioClientTest.java
@@ -7,9 +7,12 @@ import feign.jackson.JacksonDecoder;
 import org.junit.jupiter.api.Test;
 
 public class TwilioClientTest {
-    private static final String twilioSid = "ACe0da70850a27c1d2bc50dd11291450d6";
-    private static final String twilioAuthToken = "38729aba1e5b57146bb81d7918a331bd";
-    private static final String toNumber = "+40757768609";
+    @Value(${"TWILIO_SID"})
+    private static final String twilioSid;
+    @Value(${"TWILIO_AUTH_TOKEN"})
+    private static final String twilioAuthToken";
+    @Value(${"TO_NUMBER"})
+    private static final String toNumber;
     public static final String FROM_NUMBER = "+16812023405";
     public static final String TWILIO_API_DOMAIN = "https://api.twilio.com";
 
